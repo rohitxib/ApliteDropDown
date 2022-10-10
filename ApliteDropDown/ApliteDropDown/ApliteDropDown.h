15b084b5499845c5f8643c184e8d62749005e6b3
Aplite_Mac <aplite_mac@Aplite-Macs-Mac-mini.local>
2022-10-10 12:16:57 +0530

    Adding code to github

---
+++ ApliteDropDown/ApliteDropDown/ApliteDropDown.h
@@ -0,0 +1,18 @@
+//
+//  ApliteDropDown.h
+//  ApliteDropDown
+//
+//  Created by Aplite_Mac on 10/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ApliteDropDown.
+FOUNDATION_EXPORT double ApliteDropDownVersionNumber;
+
+//! Project version string for ApliteDropDown.
+FOUNDATION_EXPORT const unsigned char ApliteDropDownVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApliteDropDown/PublicHeader.h>
+
+
