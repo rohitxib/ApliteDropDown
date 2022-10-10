51efc52914e795080f4f1a0b46d563ccb709dfcc
Aplite_Mac <aplite_mac@Aplite-Macs-Mac-mini.local>
2022-10-10 15:50:26 +0530

    Added dropdown.view and dropdowncell and 2 Xib for thse

---
+++ ApliteDropDown/Dropdown/DropTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  DropTableCell.swift
+//  ApliteDropDown
+//
+//  Created by Aplite_Mac on 10/10/22.
+//
+
+import UIKit
+
+class DropTableCell: UITableViewCell {
+    @IBOutlet weak var lblTitle: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
